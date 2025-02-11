70159ba03803bdd88f4360d8639a8cd98bf74513
Jari van den Berg <jarivdberg@gmail.com>
2025-02-11 15:20:00 -0800

    chore(ltr): diff date format

--- ltr-select-date-chunk.js
+++ ltr-select-date-chunk.js
@@ -3,7 +3,7 @@ function formatDate(date) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
+    return `${month}-${day}-${year}`;
   }
   
   function parsePSTDate(dateStr) {
@@ -140,7 +140,6 @@ function formatDate(date) {
       if (availableRange.startDate && availableRange.endDate) {
         attachDateToPicker(availableRange.startDate, availableRange.endDate);
         // Sync to Wized variables
-        console.log("availableRange", availableRange);
         Wized.data.v.arrival_date = availableRange.startDate;
         Wized.data.v.departure_date = availableRange.endDate;
       } else {
