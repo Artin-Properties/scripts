c154614ce45bda0ecc270ee670853e361fad7d69
malikbasit263474 <malikbasit1104@gmail.com>
2025-02-26 00:06:49 +0500

    Update datepicker.js

--- datepicker.js
+++ datepicker.js
@@ -62,7 +62,7 @@ window.Wized.push(async (Wized) => {
         zIndex: 10,
        
         plugins: ["AmpPlugin", "RangePlugin", "LockPlugin"],
-  RangePlugin: {
+RangePlugin: {
   tooltipNumber(num) {
     if (num === 1) {
       return 1;
@@ -70,9 +70,7 @@ window.Wized.push(async (Wized) => {
     return num - 1;
   },
   locale: {
-    one: function(num) {
-      return num === 1 ? `Minimum Nights ${minNights}` : '1 night';
-    },
+    one: num === 1 ? `Minimum Nights ${minNights}` : '1 night',
     other: "nights",
   },
 },
