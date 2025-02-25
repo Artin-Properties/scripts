2dc414db16db263324af060421cc4ed12213bceb
malikbasit263474 <malikbasit1104@gmail.com>
2025-02-25 20:47:35 +0500

    Update datepicker.js

--- datepicker.js
+++ datepicker.js
@@ -60,8 +60,7 @@ window.Wized.push(async (Wized) => {
         ],
         inline: true,
         zIndex: 10,
-         minDays: result.data.minNights,
-          maxDays: result.data.maxNights,
+       
         plugins: ["AmpPlugin", "RangePlugin", "LockPlugin"],
         RangePlugin: {
           tooltip: true,
@@ -76,6 +75,8 @@ window.Wized.push(async (Wized) => {
         LockPlugin: {
           minDate: new Date(),
           inseparable: true,
+            minDays: result.data.minNights,
+          maxDays: result.data.maxNights,
           filter(date, picked) {
             const formattedDate = date.format("YYYY-MM-DD");
             const dateObj = result.data.date_object.find((obj) => obj.date === formattedDate);
