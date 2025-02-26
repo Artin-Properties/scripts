c760aa3a5a02a3f7f0f12ca5adc0c823f3b1ad5c
malikbasit263474 <malikbasit1104@gmail.com>
2025-02-26 22:57:48 +0500

    Update datepicker.js

--- datepicker.js
+++ datepicker.js
@@ -127,33 +127,25 @@ window.Wized.push(async (Wized) => {
 
                         const rangePlugin = picker.PluginManager.getInstance("RangePlugin");
 
-                         if (picker.datePicked.length !== 1) {
+                         if (picker.datePicked.length === 1) {
                              rangePlugin.options.tooltipNumber = (num) => {
+                        return num; // Show adjusted night count for the second date
+                        };
+                     let text = `Minimum Nights ${minNights}`;
+                      rangePlugin.options.locale = {
+                        one: text,
+                        };
+                         }else{
+                        rangePlugin.options.tooltipNumber = (num) => {
                         return num - 1; // Show adjusted night count for the second date
                         };
-                        let text = `night`;
-                        if (isFirstSelection) {
-
-                            text = `Minimum Nights ${minNights}`;
-                        }
+                       
+                        
                       rangePlugin.options.locale = {
                         one: "night",
                         other: "nights",
                         };
                          }
-                        rangePlugin.options.tooltipNumber = (num) => {
-                        return num; // Show adjusted night count for the second date
-                        };
-                        let text = `night`;
-                        if (isFirstSelection) {
-
-                            text = `Minimum Nights ${minNights}`;
-                        }
-                      rangePlugin.options.locale = {
-                        one: text,
-                        other: "nights",
-                        };
-                        
 
                         let firstLockedDate = null;
 
