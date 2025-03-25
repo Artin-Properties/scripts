7d189ba9cb4442bcbc2fbe4d3c9e0757b6f1fb69
Jari van den Berg <jarivdberg@gmail.com>
2025-03-25 11:18:07 -0700

    chore(invalid): invalid range

--- datepicker.js
+++ datepicker.js
@@ -271,13 +271,12 @@ window.Wized.push(async (Wized) => {
                 picker.setEndDate(endDate);
                 Wized.data.v.arrival_date = departureDateStr;
                 Wized.data.v.departure_date = departureDateStr;
+                Wized.data.v.invalidRange = true;
               } else {
                 Wized.data.v.arrival_date = arrivalDateStr;
                 Wized.data.v.departure_date = departureDateStr;
+                Wized.data.v.invalidRange = false;
                 lastEndDate = endDate;
-                // if (existingError) {
-                //   existingError.remove();
-                // }
               }
             }
           });
