2b5532717cb364241bee95834c055ecbdd0c3343
Jari van den Berg <jarivdberg@gmail.com>
2025-04-29 09:35:04 -0700

    test

--- filters/staging.js
+++ filters/staging.js
@@ -3,13 +3,13 @@ document.querySelectorAll('input[name="Property-Type"]').forEach((input) => {
     const clickedValue = input.value;
 
     // Step 1: Remove the visual active class from all radio buttons
-    document.querySelectorAll(`.properties_link-button .is-${clickedValue}`).forEach((el) => {
+    document.querySelectorAll(`.properties_link-button.is-${clickedValue}`).forEach((el) => {
       console.log("remove", el);
       el.classList.remove("w--redirected-checked");
     });
 
     // Step 3: Add the visual active class back to the matching ones
-    document.querySelectorAll(`.properties_link-button .is-${clickedValue}`).forEach((el) => {
+    document.querySelectorAll(`.properties_link-button.is-${clickedValue}`).forEach((el) => {
       console.log("add", el);
       el.classList.add("w--redirected-checked");
     });
