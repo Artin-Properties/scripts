e411c13d78654347b48ec623b32197fd8ccbc237
Jari van den Berg <jarivdberg@gmail.com>
2025-05-06 13:25:54 -0700

    fix

--- property-load/staging.js
+++ property-load/staging.js
@@ -109,7 +109,7 @@ window.Wized.push((Wized) => {
     );
 
     // If there are no property items, don't insert any mix items
-    if (Wized.data.v.property_array === 0) {
+    if (Wized.data.v.property_array.length === 0) {
       return;
     }
 
