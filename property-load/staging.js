18ba07b9155640ca5735c801e5026d9a7e0001d9
Jari van den Berg <jarivdberg@gmail.com>
2025-05-09 16:52:06 -0700

    test it again

--- property-load/staging.js
+++ property-load/staging.js
@@ -245,10 +245,6 @@ window.Wized.push((Wized) => {
     }
   }
 
-  let loaderItemsAppended = false;
-  const loaderItemHtml = `
-        <div class="properties_item loader-item"><div class="properties_card"><div class="content-shimmer"><div class="properties_card-visual is-fav"><div class="swiper is-properties-card swiper-backface-hidden swiper-initialized swiper-horizontal swiper-pointer-events"><div class="swiper-wrapper is-properties-card" id="swiper-wrapper-ba218b613edad798" aria-live="polite" style="transition-duration: 0ms; transform: translate3d(-251px, 0px, 0px);"><div class="swiper-slide is-properties-card swiper-slide-duplicate is-active swiper-slide-prev" role="group" aria-label="1 / 1" data-swiper-slide-index="0" style="width: 251px; margin-right: 0px;"><div class="properties_card-img is-dash-fav"><div class="shimmer is-square is-absolute"></div></div></div><div class="swiper-slide is-properties-card is-active swiper-slide-duplicate-next swiper-slide-duplicate-prev" role="group" aria-label="1 / 1" data-swiper-slide-index="0" style="width: 251px; margin-right: 0px;"><div class="properties_card-img is-dash-fav"><div class="shimmer is-square is-absolute"></div></div></div><div class="swiper-slide is-properties-card swiper-slide-duplicate is-active swiper-slide-next" role="group" aria-label="1 / 1" data-swiper-slide-index="0" style="width: 251px; margin-right: 0px;"><div class="properties_card-img is-dash-fav"><div class="shimmer is-square is-absolute"></div></div></div></div><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div></div><div class="properties_card-content"><div class="spacer-xsmall"></div><div class="preloader-layout gap-24"><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec46-af544784" class="shimmer is-rectangular height-20"></div></div><div class="preloader-layout gap-24"><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec48-af544784" class="shimmer is-rectangular height-20"></div><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec49-af544784" class="shimmer is-rectangular height-20"></div></div><div class="preloader-layout gap-24"><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec4b-af544784" class="shimmer is-rectangular height-12"></div><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec4c-af544784" class="shimmer is-rectangular height-12"></div></div><div class="preloader-layout gap-4"><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec4e-af544784" class="preloader-row gap-8"><div class="shimmer is-square is-tiny"></div></div><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec50-af544784" class="preloader-row gap-8"><div class="shimmer is-square is-tiny"></div></div><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec52-af544784" class="preloader-row gap-8"><div class="shimmer is-square is-tiny"></div></div><div id="w-node-_5c663453-3525-5ad6-3ff5-f2f21dc0ec54-af544784" class="preloader-row gap-8"><div class="shimmer is-square is-tiny"></div><div class="shimmer is-rectangular is-small"></div></div></div></div></div></div></div>
-         `;
   // Load more property items when the last element is visible
   async function loadMoreItems() {
     isLoading = true;
@@ -260,9 +256,12 @@ window.Wized.push((Wized) => {
     }
     lastRequestTime = now;
 
-    // Store the current scroll position and the last item's position
-    const lastItem = document.querySelector(".properties_list .properties_item:last-child");
-    const lastItemPosition = lastItem ? lastItem.getBoundingClientRect().top + window.scrollY : 0;
+    // Store the current scroll position and the last property item's position
+    const propertyItems = document.querySelectorAll('[wized="home_PropertyItem"]');
+    const lastPropertyItem = propertyItems[propertyItems.length - 1];
+    const lastItemPosition = lastPropertyItem
+      ? lastPropertyItem.getBoundingClientRect().top + window.scrollY
+      : 0;
     const currentScrollPosition = window.scrollY;
     const scrollOffset = currentScrollPosition - lastItemPosition;
 
@@ -302,14 +301,19 @@ window.Wized.push((Wized) => {
       reinitializeComponents();
 
       // Calculate new position based on the scroll offset
-      const newLastItem = document.querySelector(".properties_list .properties_item:last-child");
-      if (newLastItem) {
-        const newLastItemPosition = newLastItem.getBoundingClientRect().top + window.scrollY;
+      const newPropertyItems = document.querySelectorAll('[wized="home_PropertyItem"]');
+      const newLastPropertyItem = newPropertyItems[newPropertyItems.length - 1];
+      if (newLastPropertyItem) {
+        const newLastItemPosition =
+          newLastPropertyItem.getBoundingClientRect().top + window.scrollY;
         const newScrollPosition = newLastItemPosition + scrollOffset;
 
-        window.scrollTo({
-          top: newScrollPosition,
-          behavior: "instant",
+        // Use requestAnimationFrame to ensure DOM has updated
+        requestAnimationFrame(() => {
+          window.scrollTo({
+            top: newScrollPosition,
+            behavior: "instant",
+          });
         });
       }
 
